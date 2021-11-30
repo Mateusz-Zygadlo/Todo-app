864190ab9f9a9dd982f81c53590bef33022e768b
Mateusz <mateuszzygadlo00@gmail.com>
2021-11-30 19:03:50 +0100

    start files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-semibold flex justify-center">Hello react redux typescript tailwind app</h1>
+    </div>
+  );
+}
+
+export default App;
